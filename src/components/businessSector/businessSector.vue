dd6f39f3775b47b77e7c2d87284fda66aaec16f2
yaoyuanrong <962544785@qq.com>
2020-01-13 10:58:22 +0800

    no message

--- src/components/businessSector/businessSector.vue
+++ src/components/businessSector/businessSector.vue
@@ -7,14 +7,19 @@
 				<div class="item" v-for="(item,index) in msg" :key='index'>
 					<h3>{{item.title}}</h3>
 					<div class="buttonBox">
-						<button 
-							v-for="(item1,index1) in item.children" 
-							:key='index1' 
-							@click="route(item1.path)"
-							:class="['button',item1.type=='def'? 'defColor':'',item1.type=='imp'?'impColor':'',item1.type=='zy'? 'zyColor':'']"
-						>
-							{{item1.tit}}
-						</button>
+                        <template v-if="item.children">
+						    <button 
+						    	v-for="(item1,index1) in item.children" 
+						    	:key='index1' 
+						    	@click="route(item1.path)"
+						    	:class="['button',item1.type=='def'? 'defColor':'',item1.type=='imp'?'impColor':'',item1.type=='zy'? 'zyColor':'',item1.type == 'apply'? 'applyColor': '']"
+						    >
+						    	{{item1.tit}}
+						    </button>
+                        </template>
+                        <template v-else> 
+                            <img :src="item.img" alt="">
+                        </template>
 					</div>
 				</div>
 					
@@ -73,7 +78,35 @@
 								path:''
 							}
 						]
-					}
+					},{
+                        title: "参考资料",
+                        children: [
+                            {
+                                tit: '法律法规',
+                                type: 'def',
+                                path: ''
+                            },
+                            {
+                                tit: '国际航标',
+                                type: 'def',
+                                path: ''
+                            },
+                            {
+                                tit: '地标办法',
+                                type: 'def',
+                                path: ''
+                            },
+                            {
+                                tit: '企业标准',
+                                type: 'apply',
+                                path: ''
+                            }
+                        ]
+                    },{
+                        title: "北京城建培训教育",
+                        children: false,
+                        img:require('../../assets/home_img/trianEducation.jpg')
+                    }
 				]
 			}
 		},
